922bfd88ac3e87e64f1fc0533cafc11fa3059b58
OlukaGibson <gibsonoluka7@gmail.com>
2025-03-06 08:24:45 +0300

    optmised version

--- dailyproblem/06_03_2025/05_03_2025.py
+++ dailyproblem/06_03_2025/05_03_2025.py
@@ -17,3 +17,43 @@ class Solution:
         missing = expected_sum - (total_sum - repeated)  # Correct the sum by subtracting duplicate
         return [repeated, missing]
 
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int XOR_all = 0, XOR_grid = 0;
+        for (int num = 1; num <= n * n; ++num) {
+            XOR_all ^= num;
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                XOR_grid ^= grid[i][j];
+            }
+        }
+        int XOR_diff = XOR_all ^ XOR_grid;
+        int bit = XOR_diff & -XOR_diff;
+        int missing = 0, repeated = 0;
+        for (int num = 1; num <= n * n; ++num) {
+            if (num & bit)
+                missing ^= num;
+            else
+                repeated ^= num;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] & bit)
+                    missing ^= grid[i][j];
+                else
+                    repeated ^= grid[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == missing) swap(missing, repeated);
+            }
+        }
+
+        return {repeated, missing};
+    }
+};
