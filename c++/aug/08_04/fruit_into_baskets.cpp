29459a177688b7577270aa35bf9cb02171744e3c
OlukaGibson <gibsonoluka7@gmail.com>
2025-08-04 07:27:13 +0300

    cpp soln

---
+++ c++/aug/08_04/fruit_into_baskets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int, int> basket; // fruit type -> count
+        int left = 0;
+        int maxFruits = 0;
+
+        for (int right = 0; right < fruits.size(); ++right) {
+            basket[fruits[right]]++;
+
+            // If we have more than 2 types, shrink the window
+            while (basket.size() > 2) {
+                basket[fruits[left]]--;
+                if (basket[fruits[left]] == 0) {
+                    basket.erase(fruits[left]);
+                }
+                ++left;
+            }
+
+            // Update max fruits
+            maxFruits = max(maxFruits, right - left + 1);
+        }
+
+        return maxFruits;
+    }
+};
